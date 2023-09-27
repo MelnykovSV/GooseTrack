a0a2c228c43dbc9bfc2e16fe0a6f0c79c0b3d64e
MelnykovSV <svmelnykov@gmail.com>
2023-09-27 05:08:19 +0300

    1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Container from './App.styled';
+import { ModernNormalize } from 'emotion-modern-normalize';
+
+export const App = () => {
+  return (
+    <Container>
+      <ModernNormalize />
+      <h1>React homework template</h1>
+    </Container>
+  );
+};
