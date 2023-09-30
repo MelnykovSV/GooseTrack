5d89e1d3899cfdad7a993a076f986e0e25360d61
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-09-30 13:31:04 +0300

    add new work

--- src/components/Main/Description/Description.jsx
+++ src/components/Main/Description/Description.jsx
@@ -23,103 +23,101 @@ import desktopTask2x from '../../../images/MainPageImg/desktop_task_all@2x.png';
 
 const DescriptionSection = () => {
   return (
-    <S.DescriptionContainer>
-      <S.DescriptionSectionOne>
-        <S.DescriptionSectionTablet>
-          <S.Number>1.</S.Number>
-          <S.Title>Calendar</S.Title>
-          <S.TitleSecondary>view</S.TitleSecondary>
-          <S.Description>
-            GooseTrack's Calendar view provides a comprehensive overview of your
-            schedule, displaying all your tasks, events, and appointments in a
-            visually appealing and intuitive layout.
-          </S.Description>
-        </S.DescriptionSectionTablet>
-        <S.DescriptionPicture>
-          <source
-            srcSet={`${desktopCalendar1x} 1x, ${desktopCalendar2x} 2x`}
-            media="(min-width: 1280px)"
-          />
-          <source
-            srcSet={`${tabletCalendar1x} 1x, ${tabletCalendar2x} 2x`}
-            media="(min-width: 768px)"
-          />
-          <source
-            srcSet={`${mobileCalendar} 1x, ${mobileCalendar2x} 2x`}
-            media="(min-width: 375px)"
-          />
+    <>
+      <S.DescriptionContainer>
+        <S.DescriptionContent>
+          <S.DescriptionBlock>
+            <S.DescriptionInfo>
+              <S.DescriptionNumber>1.</S.DescriptionNumber>
+              <S.DescriptionNameAccent>calendar</S.DescriptionNameAccent>
+              <S.DescriptionName>view</S.DescriptionName>
+              <S.DescriptionText>
+                GooseTrack's Calendar view provides a comprehensive overview of
+                your schedule, displaying all your tasks, events, and
+                appointments in a visually appealing and intuitive layout.
+              </S.DescriptionText>
+            </S.DescriptionInfo>
 
-          <S.DescriptionImg
-            src={mobileCalendar}
-            srcSet={`${mobileCalendar} 1x, ${mobileCalendar2x} 2x`}
-          ></S.DescriptionImg>
-        </S.DescriptionPicture>
-      </S.DescriptionSectionOne>
-      <S.DescriptionSectionTwo>
-        <S.DescriptionSectionTablet>
-          <S.DescriptionSectionRevers>
-            <S.NumberSidebar>2.</S.NumberSidebar>
-            <S.TitleSecondarySidebar>Sidebar</S.TitleSecondarySidebar>
-            <S.Description>
-              GooseTrack offers easy access to your account settings, calendar,
-              and filters. The "My Account" section allows you to manage your
-              profile information and preferences, while the calendar provides a
-              quick and convenient way to view your upcoming events and tasks.
-            </S.Description>
-          </S.DescriptionSectionRevers>
-        </S.DescriptionSectionTablet>
-        <S.DescriptionPicture>
-          <source
-            srcSet={`${desktopSidebar1x} 1x, ${desktopSidebar2x} 2x`}
-            media="(min-width: 1280px)"
-          />
-          <source
-            srcSet={`${tabletSidebar1x} 1x, ${tabletSidebar2x} 2x`}
-            media="(min-width: 768px)"
-          />
-          <source
-            srcSet={`${mobileSidebar1x} 1x, ${mobileSidebar2x} 2x`}
-            media="(min-width: 375px)"
-          />
+            <S.DescritpionPicture>
+              <source
+                srcSet={`${desktopCalendar1x} 1x, ${desktopCalendar2x} 2x`}
+                media="(min-width: 1440px)"
+              />
+              <source
+                srcSet={`${tabletCalendar1x} 1x, ${tabletCalendar2x} 2x`}
+                media="(min-width: 768px)"
+              />
 
-          <S.DescriptionImg
-            src={mobileSidebar1x}
-            srcSet={`${mobileSidebar1x} 1x, ${mobileSidebar2x} 2x`}
-          ></S.DescriptionImg>
-        </S.DescriptionPicture>
-      </S.DescriptionSectionTwo>
-      <S.DescriptionSectionOne>
-        <S.DescriptionSectionTablet>
-          <S.Number>3.</S.Number>
-          <S.Title>All in</S.Title>
-          <S.TitleSecondary>One</S.TitleSecondary>
-          <S.Description>
-            GooseTrack is an all-in-one productivity tool that helps you stay on
-            top of your tasks, events, and deadlines. Say goodbye to scattered
-            to-do lists and hello to streamlined productivity with GooseTrack.
-          </S.Description>
-        </S.DescriptionSectionTablet>
-        <S.DescriptionPicture>
-          <source
-            srcSet={`${desktopTask1x} 1x, ${desktopTask2x} 2x`}
-            media="(min-width: 1280px)"
-          />
-          <source
-            srcSet={`${tabletTask1x} 1x, ${tabletTask2x} 2x`}
-            media="(min-width: 768px)"
-          />
-          <source
-            srcSet={`${mobileTask1x} 1x, ${mobileTask2x} 2x`}
-            media="(min-width: 375px)"
-          />
+              <source srcSet={`${mobileCalendar} 1x, ${mobileCalendar2x} 2x`} />
+              <S.DescriptionImg
+                src={mobileCalendar}
+                alt="Table calendars of three different types"
+              />
+            </S.DescritpionPicture>
+          </S.DescriptionBlock>
 
-          <S.DescriptionImg
-            src={mobileTask1x}
-            srcSet={`${mobileTask1x} 1x, ${mobileTask2x} 2x`}
-          ></S.DescriptionImg>
-        </S.DescriptionPicture>
-      </S.DescriptionSectionOne>
-    </S.DescriptionContainer>
+          <S.DescriptionBlock>
+            <S.DescriptionInfo>
+              <S.DescriptionNumber>2.</S.DescriptionNumber>
+              <S.DescriptionName>sidebar</S.DescriptionName>
+              <S.DescriptionText>
+                GooseTrack offers easy access to your account settings,
+                calendar, and filters. The "My Account" section allows you to
+                manage your profile information and preferences, while the
+                calendar provides a quick and convenient way to view your
+                upcoming events and tasks.
+              </S.DescriptionText>
+            </S.DescriptionInfo>
+            <S.DescritpionPicture>
+              <source
+                srcSet={`${desktopSidebar1x} 1x, ${desktopSidebar2x} 2x`}
+                media="(min-width: 1440px)"
+              />
+              <source
+                srcSet={`${tabletSidebar1x} 1x, ${tabletSidebar2x} 2x`}
+                media="(min-width: 768px)"
+              />
+
+              <source srcSet={`${mobileSidebar1x} 1x, ${mobileSidebar2x} 2x`} />
+              <S.DescriptionImg
+                src={mobileSidebar1x}
+                alt="User panel with dark and light themes"
+              />
+            </S.DescritpionPicture>
+          </S.DescriptionBlock>
+
+          <S.DescriptionBlock>
+            <S.DescriptionInfo>
+              <S.DescriptionNumber>3.</S.DescriptionNumber>
+              <S.DescriptionNameAccent>all in</S.DescriptionNameAccent>
+              <S.DescriptionName>one</S.DescriptionName>
+              <S.DescriptionText>
+                GooseTrack is an all-in-one productivity tool that helps you
+                stay on top of your tasks, events, and deadlines. Say goodbye to
+                scattered to-do lists and hello to streamlined productivity with
+                GooseTrack.
+              </S.DescriptionText>
+            </S.DescriptionInfo>
+            <S.DescritpionPicture>
+              <source
+                srcSet={`${desktopTask1x} 1x, ${desktopTask2x} 2x`}
+                media="(min-width: 1440px)"
+              />
+              <source
+                srcSet={`${tabletTask1x} 1x, ${tabletTask2x} 2x`}
+                media="(min-width: 768px)"
+              />
+
+              <source srcSet={`${mobileTask1x} 1x, ${mobileTask2x} 2x`} />
+              <S.DescriptionImg
+                src={mobileTask1x}
+                alt="Part of the screen with the calendar and scheduled tasks"
+              />
+            </S.DescritpionPicture>
+          </S.DescriptionBlock>
+        </S.DescriptionContent>
+      </S.DescriptionContainer>
+    </>
   );
 };
 
--- src/components/Main/Description/Description.styled.jsx
+++ src/components/Main/Description/Description.styled.jsx
@@ -19,160 +19,125 @@ export const DescriptionContainer = styled.div`
   }
 `;
 
-export const Number = styled.h2`
-  color: #3e85f3;
-  font-family: Inter;
-  font-size: 80px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 100%; /* 80px */
-  letter-spacing: -4px;
-  margin-top: 64px;
-  margin-bottom: 22px;
-  // width: 56px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 104px;
+export const DescriptionContent = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 64px 20px;
+  margin-top: 0;
   }
 
-  @media screen and (min-width: 1440px) {
-  }
-`;
-
-export const NumberSidebar = styled.h2`
-  color: #3e85f3;
-  font-family: Inter;
-  font-size: 80px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 100%; /* 80px */
-  letter-spacing: -4px;
-  margin-top: 64px;
-  margin-bottom: 14px;
-  // width: 56px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 104px;
+  & > li:nth-of-type(2) > :first-of-type {
+    margin-left: auto;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media (min-width: 768px) {
+    padding: 64px 32px;
+  }
+  @media (min-width: 1440px) {
+    padding: 64px 128px;
   }
 `;
 
-export const Title = styled.h2`
-  color: #3e85f3;
-  font-family: Inter;
-  font-size: 32px;
-  font-style: normal;
+export const DescriptionBlock = styled.li`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 64px;
+  @media (min-width: 1440px) {
+    flex-direction: row;
+    gap: 228px;
+    margin-left: auto;
+
+    &:nth-of-type(2) {
+      flex-direction: row-reverse;
+      margin-right: auto;
+      margin-left: 0;
+    }
+  }
+`;
+
+export const DescriptionInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    max-width: 275px;
+  }
+`;
+
+export const DescriptionNumber = styled.h2`
+  display: flex;
+  font-size: 80px;
   font-weight: 700;
-  line-height: 40px; /* 125% */
-  text-transform: uppercase;
+  line-height: 0.8;
+  letter-spacing: -4px;
+  color: #3e85f3;
+  margin: 0;
+`;
+
+export const DescriptionNameAccent = styled.h2`
+  display: inline-block;
+  width: fit-content;
+  margin-top: 20px;
+  margin-bottom: 0;
   padding: 8px 18px;
-  background-color: #dcebf7;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 1.25;
+  color: #3e85f3;
+  text-transform: uppercase;
   border-radius: 44px;
-  // width: 214px;
+  background-color: #dcebf7;
 
-  @media screen and (min-width: 768px) {
+  @media (min-width: 768px) {
+    line-height: 1.1;
+    padding: 6px 18px;
+    max-width: 264px;
     font-size: 40px;
-  }
-
-  @media screen and (min-width: 1440px) {
   }
 `;
 
-export const TitleSecondary = styled.h2`
-  color: #171820;
-  font-family: Inter;
+export const DescriptionName = styled.h3`
   font-size: 32px;
-  font-style: normal;
   font-weight: 700;
-  line-height: 40px; /* 125% */
-  text-transform: uppercase;
-  // width: 86px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 40px;
-  }
-
-  @media screen and (min-width: 1440px) {
-  }
-`;
-
-export const TitleSecondarySidebar = styled.h2`
+  line-height: 1.25;
   color: #171820;
-  font-family: Inter;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 40px; /* 125% */
   text-transform: uppercase;
-  // width: 86px;
-  margin: 0px 0px 14px 0px;
+  margin-top: 8px;
+  margin-bottom: 0;
 
-  @media screen and (min-width: 768px) {
+  @media (min-width: 768px) {
+    line-height: 1.1;
     font-size: 40px;
-  }
-
-  @media screen and (min-width: 1440px) {
   }
 `;
 
-export const Description = styled.p`
-  color: rgba(17, 17, 17, 0.9);
-  font-family: Inter;
+export const DescriptionText = styled.p`
   font-size: 14px;
-  font-style: normal;
   font-weight: 500;
-  line-height: 18px; /* 128.571% */
-  width: 335px;
+  line-height: 1.28;
+  color: #111111e5;
+  margin-bottom: 40px;
   margin-top: 14px;
-
-  @media screen and (min-width: 768px) {
-    width: 275px;
-  }
-
-  @media screen and (min-width: 1440px) {
+  @media (min-width: 768px) {
+    margin-top: 24px;
   }
 `;
 
-export const DescriptionPicture = styled.picture`
-  width: 335px;
-  height: 457px;
-`;
-
-export const DescriptionImg = styled.img`
+export const DescritpionPicture = styled.picture`
   width: 335px;
   height: 457px;
 
-  @media screen and (min-width: 768px) {
+  @media (min-width: 768px) {
     width: 704px;
     height: 700px;
   }
-
-  @media screen and (min-width: 1440px) {
+  @media (min-width: 1440px) {
     width: 604px;
     height: 700px;
   }
 `;
 
-export const DescriptionSectionOne = styled.section`
-  @media screen and (min-width: 1440px) {
-    display: flex;
-    max-width: 1107px;
-    margin-top: 64px;
-    gap: 228px;
-  }
+export const DescriptionImg = styled.img`
+  width: 100%;
+  height: 100%;
 `;
-
-export const DescriptionSectionTwo = styled.section``;
-export const DescriptionSectionThree = styled.section``;
-
-export const DescriptionSectionRevers = styled.div`
-  @media screen and (min-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-  }
-`;
-
-export const DescriptionSectionTablet = styled.div``;
