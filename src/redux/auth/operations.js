68a6fdc5a10176c08207a1b50fd990b721348408
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-10-03 23:26:09 +0200

    .

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -83,10 +83,11 @@ export const updateAvatar = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const response = await privateApi.patch('/api/auth/avatar');
+      Notify.success('User avatar updated successfully!');
       console.log(response);
       return credentials.data.data;
     } catch (error) {
-      Notify.failure(`Something went wrong: ${error.message}`);
+      // Notify.failure(`Something went wrong: ${error.message}`);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -97,7 +98,6 @@ export const editData = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const response = await privateApi.patch('/api/auth/user', credentials);
-      console.log(response);
       Notify.success('User data updated successfully!');
       return response.data.data;
     } catch (error) {
