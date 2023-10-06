cedfab58d2f27ef76eef87d2c6f64b575110de05
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2023-10-06 17:37:39 +0300

    added avatar component to header

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,21 +1,17 @@
 import { useSelector } from 'react-redux';
 import { getUser } from 'redux/auth/authSlice';
-import { UserImg, UserInfoWrapper, UserName } from './UserInfo.styled';
-// import { Avatar } from 'components/Avatar/Avatar';
+import { AvatarWrapper, UserInfoWrapper, UserName } from './UserInfo.styled';
+import { Avatar } from 'components/Avatar/Avatar';
 
 export const UserInfo = () => {
   const user = useSelector(getUser);
 
   return (
     <UserInfoWrapper>
-      <UserName>{user.userName}</UserName>
-      {/* <Avatar
-        style={{
-          width: '32px',
-          height: '32px',
-        }}
-      /> */}
-      <UserImg src={user.avatarUrl} alt="user" />
+      <UserName>{user.userName}ddddddd</UserName>
+      <AvatarWrapper>
+        <Avatar />
+      </AvatarWrapper>
     </UserInfoWrapper>
   );
 };
