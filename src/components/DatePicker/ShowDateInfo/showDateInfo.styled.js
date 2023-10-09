468068c9c0fdbbfbdb62adf3cb9809297f0881da
MelnykovSV <svmelnykov@gmail.com>
2023-10-09 05:54:21 +0300

    styles

--- src/components/DatePicker/ShowDateInfo/showDateInfo.styled.js
+++ src/components/DatePicker/ShowDateInfo/showDateInfo.styled.js
@@ -1,20 +1,30 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 
 export const Container = styled.div`
-
-
   .dateBox {
-    width: 704px;
+    width: 100%;
     height: 68px;
-    border: 1px solid rgba(220, 227, 229, 0.8);
+    border: 1px solid ${({ theme }) => theme.strokeTransparant};
     display: flex;
     border-radius: 8px;
     align-items: center;
     margin-top: 10px;
+    margin-bottom: 14px;
+    @media screen and (min-width: 768px) {
+      margin-bottom: 16px;
+    }
   }
 
   .dayOfWeek {
     text-align: center;
+    padding: 4px 6px;
+    color: ${({ theme }) => theme.textPrimaryLight};
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 18px; /* 112.5% */
+    text-transform: uppercase;
   }
 
   .dateNumber {
@@ -35,6 +45,18 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     display: flex;
+    &.day {
+      cursor: pointer;
+    }
+  }
+  .dayContainer.current {
+    pointer-events: none;
+
+    .dayOfWeek {
+      border-radius: 6px;
+      background-color: #3e85f3;
+      color: white;
+    }
   }
 
   .containerMonth {
@@ -50,8 +72,22 @@ export const Container = styled.div`
   .dateText {
     text-transform: uppercase;
     margin: 0;
-  }
+    margin-bottom: 6px;
 
+    color: ${({ theme }) => theme.userProfileLabel};
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: calc(18 / 16);
+    text-transform: uppercase;
+  }
+  .dateText.month {
+    color: ${({ theme }) => theme.textPrimaryLight};
+  }
+  .dateText.month.weakend {
+    color: ${({ theme }) => theme.weakend};
+  }
 
   .container {
     max-width: 1440px;
--- src/pages/CalendarDatepicerPage/CalendarDatepickerPage.jsx
+++ src/pages/CalendarDatepicerPage/CalendarDatepickerPage.jsx
@@ -4,7 +4,6 @@ import { DatePicker } from 'components/DatePicker/DatePicker/datePicker';
 export const CalendarDatepickerPage = () => {
   return (
     <div>
-      <h2>Datepicker will be here</h2>
       <DatePicker />
       <Outlet />
     </div>
