bc515d3111f9c3d91fb77b66957f7dd9b9a0df02
MelnykovSV <svmelnykov@gmail.com>
2023-10-09 12:48:13 +0300

    minor fixes

--- src/components/UserSectionWrap/UserForm/constants/userFormValidation.js
+++ src/components/UserSectionWrap/UserForm/constants/userFormValidation.js
@@ -1,10 +1,16 @@
 import { object, string, date } from 'yup';
+import { userNameRegexp } from 'regExp';
 
 export const userFormValidation = object({
   userName: string()
     .required('Please enter your name')
     .min(3, 'Too short!')
-    .max(16, 'Too long!'),
+    .max(16, 'Too long!')
+    .matches(
+      userNameRegexp,
+      'Your username should consist of letters, numbers, and underscores only'
+    ),
+
   birthday: date(),
   email: string()
     .required('Please enter your email.')
